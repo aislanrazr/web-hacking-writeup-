f85d3c52191de714de8c13bc124f61c6216780f9
aislanrazr <aislanrazr@gmail.com>
2025-12-10 14:04:10 -0300

    Update simulated_cmd_injection_poc.py

--- poc/simulated_cmd_injection_poc.py
+++ poc/simulated_cmd_injection_poc.py
@@ -1,1 +1,19 @@
-print('cmd inj')
+"""
+PoC SIMULADA — Command Injection
+Detecta padrões suspeitos, sem executar comandos.
+"""
+
+import re
+
+def detect_injection(data):
+    pattern = re.compile(r"[;&|`]")
+    return bool(pattern.search(data))
+
+inputs = [
+    "example.com",
+    "example.com; ls",
+    "8.8.8.8 | whoami"
+]
+
+for i in inputs:
+    print(i, "-> suspeito?", detect_injection(i))
